6c893d84c26fe1ac6ee2d93a5c2f2a3a952d7de6
Christian Cyrille Villaseñor <75314757+cyrll29@users.noreply.github.com>
2024-11-26 23:24:42 +0800

    fix: fixed dynamic route

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,3 +1,4 @@
+import Sort from '@/components/Sort';
 import React from 'react'
 
 const page = async ({ params }: SearchParamProps) => {
@@ -7,6 +8,17 @@ const page = async ({ params }: SearchParamProps) => {
         <div className="page-container">
             <section className="w-full">
                 <h1 className='h1 capitalize'>{type}</h1>
+                <div className="total-size-section">
+                    <p className="body-1">
+                        Total : <span className='h5'>0 MB</span>
+                    </p>
+                    <div className='sort-container'>
+                        <p className='body-1 hidden sm:block text-light-200'>
+                            Sort by:
+                        </p>
+                        <Sort />
+                    </div>
+                </div>
             </section>
         </div>
     )
