1ecdfdff0c46d52b0f83f4903b9cbab7425164ba
Christian Cyrille Villaseñor <75314757+cyrll29@users.noreply.github.com>
2024-11-28 08:02:05 +0800

    fix: fix !session error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -82,19 +82,24 @@ export const verifySecret = async ({ accountId, password}: { accountId: string, 
 }
 
 export const getCurrentUser = async () => {
-    const { databases, account } = await createSessionClient();
 
-    const result = await account.get();
+    try {
+        const { databases, account } = await createSessionClient();
 
-    const user = await databases.listDocuments(
-        appwriteConfig.databaseId,
-        appwriteConfig.usersCollectionId,
-        [Query.equal("accountId", [result.$id])],
-    )
+        const result = await account.get();
 
-    if(user.total <= 0) return null;
+        const user = await databases.listDocuments(
+            appwriteConfig.databaseId,
+            appwriteConfig.usersCollectionId,
+            [Query.equal("accountId", [result.$id])],
+        )
 
-    return parseStringify(user.documents[0]);
+        if(user.total <= 0) return null;
+
+        return parseStringify(user.documents[0]);
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export const signOutUser = async () => {
